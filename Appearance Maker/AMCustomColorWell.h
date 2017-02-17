49e94e465a593366e5b5d35b90de84ca7c488f7a
leo <leo.lch@alibaba-inc.com>
2017-02-17 15:07:01 +0800

    make colorwell could select alpha value

---
+++ Appearance Maker/AMCustomColorWell.h
@@ -0,0 +1,13 @@
+//
+//  AMCustomColorWell.h
+//  Appearance Maker
+//
+//  Created by leo on 2017/2/17.
+//  Copyright © 2017年 Guilherme Rambo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AMCustomColorWell : NSColorWell
+
+@end
